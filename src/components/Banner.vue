ff57f128f96ed59d987ceb21454236613f10e4ea
Thais Silveira <88152671+thaissilvr@users.noreply.github.com>
2022-03-28 21:01:08 -0300

    Criação do banner do projeto

---
+++ src/components/Banner.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="main-banner">
+        <h1>Make your burguer</h1>
+    </div>    
+</template>
+
+<script>
+export default {
+    name: "Banner"
+}
+</script>
+
+<style scoped>
+#main-banner {
+  background: url("https://github.com/thaissilvr/makeyour-burguer-vue3/blob/main/public/img/burger.jpg?raw=true");
+  background-position: right bottom -200px;
+  background-size: cover;
+  height: 500px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+}
+#main-banner h1 {
+  color: #fff;
+  text-align: center;
+  font-size: 50px;
+  background-color: #222;
+  padding: 20px 40px;
+}
+@media (max-width: 1061px) {
+  #main-banner {
+    background-position: right bottom;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,22 @@
 <template>
+<Banner />
   <div class="main-container">
-    <h1>HOME</h1>
+    <h1>Monte seu burguer:</h1>
+    <p>form</p>
+
   </div>
 </template>
 
 
 
 <script>
+
+import Banner from '../components/Banner.vue'
 export default {
+  name: "Home",
+  components: {
+    Banner
+  }
 
 }
 </script>
